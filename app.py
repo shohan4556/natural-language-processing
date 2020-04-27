958eebd5afaee2647f3b68a91401645bfa2bac34
shohan <shohan4556@gmail.com>
2020-04-27 23:15:42 +0600

    regular expression

--- app.py
+++
@@ -1,8 +0,0 @@
-import re 
-
-text = 'abcded efgh'
-my_string = "Let's write RegEx!"
-
-print(re.match('abc', text))
-print(re.split('a', text))
-print(re.findall('\w+', my_string))
---
+++ regular_exp.py
@@ -0,0 +1,31 @@
+import re 
+
+text = 'abcded efgh'
+my_string = "Let's write RegEx!"
+
+print(re.match(r'abc', text))
+print(re.split(r'a', text))
+
+# find all words 
+print(re.findall(r'\w*', my_string))
+
+my_string = "Let's write RegEx!  Won't that be fun?  I sure think so.  Can you find 4 sentences?  Or perhaps, all 19 words?"
+
+# Write a pattern to match sentence endings: sentence_endings
+sentence_endings = r"[.?!]"
+
+# Split my_string on sentence endings and print the result
+print(re.split(sentence_endings, my_string))
+
+# Find all capitalized words in my_string and print the result
+capitalized_words = r"[A-Z]\w+"
+print(re.findall(capitalized_words, my_string))
+
+# Split my_string on spaces and print the result
+spaces = r"\s+"
+print(re.split(spaces, my_string))
+
+# Find all digits in my_string and print the result
+digits = r"\d+"
+print(re.findall(digits, my_string))
+
