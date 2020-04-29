9a4565bb59babec7ffb4f41dc6e146ea3585c454
shohan <shohan4556@gmail.com>
2020-04-29 13:09:00 +0600

    simple bag of word

---
+++ bow_simple.py
@@ -0,0 +1,18 @@
+# Import Counter
+from collections import Counter 
+from nltk.tokenize import word_tokenize
+
+
+article = 'this is a demo article, demo article of wikipedia'
+
+# Tokenize the article: tokens
+tokens = word_tokenize(article)
+
+# Convert the tokens into lowercase: lower_tokens
+lower_tokens = [i.lower() for i in tokens]
+
+# Create a Counter with the lowercase tokens: bow_simple
+bow_simple = Counter(lower_tokens)
+
+# Print the 10 most common tokens
+print(bow_simple.most_common(2))
